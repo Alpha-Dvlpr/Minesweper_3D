d3a12cd41ade2b9d4e30d612b63b728097b3d218
Alpha-Dvlpr <alpha.dvlpr@gmail.com>
2021-05-09 15:15:09 +0200

    Initial Commit

---
+++ Minesweeper 3D/Minesweeper_3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Minesweeper_3DApp.swift
+//  Minesweeper 3D
+//
+//  Created by Aaron Granado Amores on 09/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Minesweeper_3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
