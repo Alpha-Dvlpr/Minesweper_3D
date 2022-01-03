6b9f0f3ad4db05c6bdcb9130e87e0655e933107d
Aarón Granado <agrana16@corp.vodafone.es>
2022-01-03 15:43:03 +0100

    Temporarily disabled hint action button + button size fix

--- Minesweeper 3D/Modules/Game/Views/ActionsVC.swift
+++ Minesweeper 3D/Modules/Game/Views/ActionsVC.swift
@@ -20,22 +20,22 @@ struct ActionsVC: View {
                 self.update(image: Images.numbers(1), action: .number)
                 self.update(image: Images.symbols(.flag), action: .flag)
                 self.update(image: Images.symbols(.mine), action: .mine)
-                GeometryReader { shape in
-                    self.update(image: Images.symbols(.hint), action: .hint)
-                        .overlay(
-                            self.update(
-                                image: Images.numbers(Constants.numberOfHints),
-                                action: .hint
-                            )
-                            .background(Color.white)
-                            .clipShape(Circle())
-                            .frame(
-                                width: shape.size.width * 0.4,
-                                height: shape.size.height * 0.4),
-                            alignment: .topTrailing
-                        )
-                }
-                .aspectRatio(1.0, contentMode: .fit)
+//                GeometryReader { shape in
+//                    self.update(image: Images.symbols(.hint), action: .hint)
+//                        .overlay(
+//                            self.update(
+//                                image: Images.numbers(Constants.numberOfHints),
+//                                action: .hint
+//                            )
+//                            .background(Color.white)
+//                            .clipShape(Circle())
+//                            .frame(
+//                                width: shape.size.width * 0.4,
+//                                height: shape.size.height * 0.4),
+//                            alignment: .topTrailing
+//                        )
+//                }
+//                .aspectRatio(1.0, contentMode: .fit)
             }
             Spacer().frame(width: 30)
         }
@@ -44,6 +44,7 @@ struct ActionsVC: View {
     private func update(image: Images, action: Action) -> some View {
         return image.image
             .resizable()
+            .frame(width: 60, height: 60)
             .aspectRatio(1.0, contentMode: .fit)
             .foregroundColor(self.color)
             .onTapGesture { if self.enabled { self.callback(action) } }
