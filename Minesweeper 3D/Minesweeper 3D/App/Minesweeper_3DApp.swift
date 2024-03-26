77aa80327540286ae3bc1cf4c9502ea8ad0c68ad
agranado <aaron.granado@i3television.es>
2024-03-26 16:04:19 +0100

    Created new base project

---
+++ Minesweeper 3D/Minesweeper 3D/App/Minesweeper_3DApp.swift
@@ -0,0 +1,18 @@
+//
+//  Minesweeper_3DApp.swift
+//  Minesweeper 3D
+//
+//  Created by Aarón Granado Amores on 26/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Minesweeper_3DApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            Text("Hello")
+        }
+    }
+}
